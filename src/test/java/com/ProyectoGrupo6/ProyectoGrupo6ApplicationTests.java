0c4d87f20adf4cfb560cafb6285b1211d6a35fd1
JunCode <109869846+JunCold@users.noreply.github.com>
2023-07-05 12:55:50 -0600

    Creacion del Proyecto

---
+++ src/test/java/com/ProyectoGrupo6/ProyectoGrupo6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoGrupo6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGrupo6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
