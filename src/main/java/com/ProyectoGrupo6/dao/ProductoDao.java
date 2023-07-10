69be3f518e0dba1cb60db4a0824083eafc542659
Valeria_Arias_Castro <107090508+Pinguinita1102@users.noreply.github.com>
2023-07-10 17:36:06 -0600

    Parte de la interfaz 2

--- src/main/java/com/ProyectoGrupo6/dao/ProductoDao.java
+++ src/main/java/com/ProyectoGrupo6/dao/ProductoDao.java
@@ -2,6 +2,7 @@
 package com.ProyectoGrupo6.dao;
 import com.ProyectoGrupo6.domain.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface ProductoDao extends JpaRepository<Producto, String>{
     
 }
